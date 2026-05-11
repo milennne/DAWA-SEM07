1c91e04dada0566cc5d97331bddf5d9ba43fe4ab
milennne <milene.fuentes@tecsup.edu.pe>
2026-05-10 19:06:36 -0500

    Listo para desplegar

--- app/models/index.js
+++ app/models/index.js
@@ -7,7 +7,9 @@ const sequelize = new Sequelize(
   dbConfig.PASSWORD,
   {
     host: dbConfig.HOST,
-    dialect: dbConfig.dialect
+    port: dbConfig.PORT,
+    dialect: dbConfig.dialect,
+    dialectOptions: dbConfig.dialectOptions
   }
 );
 
